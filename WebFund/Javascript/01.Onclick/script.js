0d45fafddcb2b4dfa5c0cfab5b3b45ee1b759a2c
Rhayza Yepez <rhayzayepez@gmail.com>
2023-06-14 16:43:13 -0400

    actualizando

--- WebFund/Javascript/01.Onclick/script.js
+++ WebFund/Javascript/01.Onclick/script.js
@@ -1,4 +1,14 @@
+function cambiarNombre() {
+  var boton = document.getElementById("boton-login");
+  boton.innerText = "Logout";
+}
+
 function handleClick() {
   alert("Ninja was liked");
   console.log("Ninja was liked")
+}
+
+function ocultarBoton() {
+  var boton = document.getElementById("definition");
+  boton.style.display = "none";
 }
