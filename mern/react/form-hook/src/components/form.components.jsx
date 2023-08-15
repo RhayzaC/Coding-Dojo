274f2e03673e9b4bd7db00abaea67a0cf29a5e0d
Rhayza Yepez <rhayzayepez@gmail.com>
2023-08-15 14:59:02 -0400

    formulario completo actualizado

--- mern/react/form-hook/src/components/form.components.jsx
+++ mern/react/form-hook/src/components/form.components.jsx
@@ -1,66 +1,164 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Form = (props) => {
-  let [info, setinfo] = useState({
-    firstName: "",
-    lastName: "",
+function Form() {
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
     email: "",
-    password: "",
-    confirmPassword: ""
+    contraseña: "",
+    confirmarContraseña: "",
   });
 
-  const handleChange = (event) => {
-    const key = event.target.name;
-    const value = event.target.value;
-    setinfo({
-      ...info,
-      [key]: value,
-    });
+  const [validationErrors, setValidationErrors] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    // Validaciones//
+    if (name === "nombre" && value.length < 2) {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        nombre: "El nombre debe tener al menos 2 caracteres.",
+      }));
+    } else if (name === "apellido" && value.length < 2) {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        apellido: "El apellido debe tener al menos 2 caracteres.",
+      }));
+    } else if (name === "email" && value.length < 5) {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "El correo electrónico debe tener al menos 5 caracteres.",
+      }));
+    } else if (name === "contraseña" && value.length < 8) {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        contraseña: "La contraseña debe tener al menos 8 caracteres.",
+      }));
+    } else if (
+      name === "confirmarContraseña" &&
+      value !== formData.contraseña
+    ) {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        confirmarContraseña: "Las contraseñas no coinciden.",
+      }));
+    } else {
+      setValidationErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: "",
+      }));
+    }
   };
 
   return (
-    <div>
-      <form style={{ margin: '50px' }}>
-        <div style={{backgroundColor: 'lightgray',}}>
-          <label className="form-label"> First Name:</label>
-          <input type="text" name="firstName" value={info.firstName} onChange={handleChange} 
-          style={{ marginBottom: '25px', width: '20%'}} />
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6">
+          <form>
+            <div className="mb-3 bg-light p-3">
+              <div className="row justify-content-center">
+                <div className="col-md-8 form in-line">
+                  <label className="form-label"> First Name:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="nombre"
+                    value={formData.nombre}
+                    onChange={handleChange}
+                  />
+                  {validationErrors.nombre && (
+                    <div className="text-danger">{validationErrors.nombre}</div>
+                  )}
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <label className="form-label"> Last Name: </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="apellido"
+                    value={formData.apellido}
+                    onChange={handleChange}
+                  />
+                  {validationErrors.apellido && (
+                    <div className="text-danger">
+                      {validationErrors.apellido}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <label className="form-label"> Email:</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                  {validationErrors.email && (
+                    <div className="text-danger">{validationErrors.email}</div>
+                  )}
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <label className="form-label"> Password: </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="contraseña"
+                    value={formData.contraseña}
+                    onChange={handleChange}
+                  />
+                  {validationErrors.contraseña && (
+                    <div className="text-danger">
+                      {validationErrors.contraseña}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <label className="form-label"> Confirm Password: </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="confirmarContraseña"
+                    value={formData.confirmarContraseña}
+                    onChange={handleChange}
+                  />
+                  {validationErrors.confirmarContraseña && (
+                    <div className="text-danger">
+                      {validationErrors.confirmarContraseña}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </form>
+          <div className="mt-4">
+            <h5>Your Data:</h5>
+            <p>First Name: {formData.nombre}</p>
+            <p>Last Name: {formData.apellido}</p>
+            <p>Email: {formData.email}</p>
+            <p>Password: {formData.contraseña}</p>
+            <p>Confirm Password: {formData.confirmarContraseña}</p>
+          </div>
         </div>
-
-        <div style={{backgroundColor: 'lightgray',}}>
-          <label className="form-label"> Last Name:</label>
-          <input type="text" name="lastName" value={info.lastName} onChange={handleChange} 
-          style={{ marginBottom: '25px', width: '20%' }} />
-        </div>
-
-        <div style={{backgroundColor: 'lightgray',}}>
-          <label className="form-label"> Email:</label>
-          <input type="email" name="email" value={info.email} onChange={handleChange} 
-          style={{ marginBottom: '25px', width: '20%' }} />
-        </div>
-
-        <div style={{backgroundColor: 'lightgray',}}>
-          <label className="form-label"> Password:</label>
-          <input type="password" name="password" value={info.password} onChange={handleChange}
-          style={{ marginBottom: '25px', width: '20%' }} />
-        </div>
-
-        <div style={{backgroundColor: 'lightgray',}}>
-          <label className="form-label"> Confirm Password:</label>
-          <input type="password" name="confirmPassword" value={info.confirmPassword} onChange={handleChange}
-          style={{ marginBottom: '25px', width: '20%' }}/>
-        </div>
-      </form>
-
-      <h4>Your Form Data:</h4>
-      <p>First Name: {info.firstName}</p>
-      <p>Last Name: {info.lastName}</p>
-      <p>Email: {info.email}</p>
-      <p>Password: {info.password}</p>
-      <p>Confirm Password: {info.confirmPassword}</p>
+      </div>
     </div>
   );
-};
-
+}
 export default Form;
-
