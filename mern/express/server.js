b0d5adf0977777209daf2f9e18350e29302b201b
Rhayza Yepez <rhayzayepez@gmail.com>
2023-08-16 04:36:32 -0400

    actualizando fake api

--- mern/express/server.js
+++ mern/express/server.js
@@ -1,33 +1,95 @@
 const express = require("express");
-const faker = require("faker");
+const { faker } = require("@faker-js/faker");
+//import { faker } from "@faker-js/faker";
+
 
 const app = express();
-const PORT = 8000;
+const port = 8000;
 
-// Endpoint para obtener un usuario aleatorio
-app.get("/user", (req, res) => {
-    const user = {
-    name: faker.name.findName(),
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Classes//
+
+class User {
+  constructor() {
+    this.firstName = faker.person.firstName();
+    this.lastName = faker.person.lastName();
+    this.phoneNumber = faker.phone.number();
+    this.email = faker.internet.email();
+    this.password = faker.internet.password();
+  }
+}
+
+class Company {
+  constructor() {
+    this.name = faker.company.name();
+    this.address = {
+      street: faker.location.street(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      zipCode: faker.location.zipCode(),
+      country: faker.location.country(),
+    };
+  }
+}
+
+// Functions// 
+
+const createUser = () => {
+  const newUser = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    phoneNumber: faker.phone.number(),
     email: faker.internet.email(),
-    address: faker.address.streetAddress(),
-    phone: faker.phone.phoneNumber(),
+    password: faker.internet.password(),
+  };
+  return newUser;
 };
 
-    res.json(user);
-});
-
-// Endpoint para obtener una compañía aleatoria
-app.get("/company", (req, res) => {
-    const company = {
-    name: faker.company.companyName(),
-    industry: faker.company.catchPhrase(),
-    address: faker.address.streetAddress(),
-    phone: faker.phone.phoneNumber(),
+const createCompany = () => {
+  const newCompany = {
+    name: faker.company.name(),
+    address: {
+      street: faker.location.streetAddress(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      zipCode: faker.location.zipCode(),
+      country: faker.location.country(),
+    },
+  };
+  return newCompany;
 };
 
-    res.json(company);
+// Route for creating a new user
+app.post("/api/users/new", (req, res) => {
+  let newUserCls = new User();
+  let newUSerFunc = createUser();
+  res.json({ userCls: newUserCls, userFunc: newUSerFunc });
 });
 
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
+// Route for creating a new company
+app.post("/api/companies/new", (req, res) => {
+  let newCompanyCls = new Company();
+  let newCompanyFunc = createCompany();
+  res.json({ companyCls: newCompanyCls, companyFunc: newCompanyFunc });
 });
+
+// Route for creating a new user and a new company
+app.post("/api/user/company", (req, res) => {
+  let newUserCls = new User();
+  let newCompanyCls = new Company();
+  let newUserFunc = createUser();
+  let newCompanyFunc = createCompany();
+  res.json({
+    userCls: newUserCls,
+    companyCls: newCompanyCls,
+    userFunc: newUserFunc,
+    companyFunc: newCompanyFunc,
+  });
+});
+
+
+// Setting up the server to listen on port 8000
+app.listen(port, () => console.log(`Listening on port: ${port}`));
